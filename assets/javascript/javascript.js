b904ef587ad4aff434f0f322261050d4272ccce2
Noah Sylwester <nanoabram@comcast.net>
2019-08-28 23:46:54 -0700

    add basic html css structure with bootstrap.

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,8 @@
+// var connectedRef = firebase.database().ref(".info/connected");
+// connectedRef.on("value", function(snap) {
+//   if (snap.val() === true) {
+//     alert("connected");
+//   } else {
+//     alert("not connected");
+//   }
+// });
