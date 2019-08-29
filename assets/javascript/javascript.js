4f975b337d8deadea45a8eaf5c0842aa34d4ff7a
Noah Sylwester <nanoabram@comcast.net>
2019-08-29 01:20:04 -0700

    Add html structure, css, images.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,3 +1,16 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCyRdnTBWciurlZKeh00AeqDzyLhxW3MLQ",
+  authDomain: "multiplayer-rps-dbf7f.firebaseapp.com",
+  databaseURL: "https://multiplayer-rps-dbf7f.firebaseio.com",
+  projectId: "multiplayer-rps-dbf7f",
+  storageBucket: "multiplayer-rps-dbf7f.appspot.com",
+  messagingSenderId: "481817965714",
+  appId: "1:481817965714:web:5cdc0ad35e70c912"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 // var connectedRef = firebase.database().ref(".info/connected");
 // connectedRef.on("value", function(snap) {
 //   if (snap.val() === true) {
