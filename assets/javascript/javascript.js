a79baf3e56e9c8d9fc78de7efb24403428a19436
Noah Sylwester <nanoabram@comcast.net>
2019-08-30 13:08:08 -0700

    wip, adding firebase functionality.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -11,11 +11,57 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// var connectedRef = firebase.database().ref(".info/connected");
-// connectedRef.on("value", function(snap) {
-//   if (snap.val() === true) {
-//     alert("connected");
-//   } else {
-//     alert("not connected");
-//   }
-// });
+var isPlayerOneConnected = false;
+var isPlayerTwoConnected = false;
+var playerOneChoice = "";
+var playerTwoChoice = "";
+
+var database = firebase.database();
+var connectedRef = database.ref(".info/connected");
+
+connectedRef.on("value", function(snap) {
+  if (snap.val() === true) {
+    alert("connected");
+  } else {
+    alert("not connected");
+  }
+});
+
+
+
+
+// On Click
+$("#click-button").on("click", function() {
+
+  // Add 1 to clickCounter
+  clickCounter++;
+
+  // **** Store Click Data to Firebase in a JSON property called clickCount *****
+  // **** Note how we are using the Firebase .set() method ****
+  // **** .ref() refers to the path you want to save your data to
+  // **** Since we left .ref() blank, it will save to the root directory
+  database.ref().set({
+    clickCount: clickCounter,
+    codeQuality: "super-excellent-wonderful"
+  });
+});
+
+database.ref().on("value", function(snapshot) {
+
+  // Log everything that's coming out of snapshot
+  console.log(snapshot.val());
+  console.log(snapshot.val().name);
+  console.log(snapshot.val().email);
+  console.log(snapshot.val().age);
+  console.log(snapshot.val().comment);
+
+  // Change the HTML to reflect
+  $("#name-display").text(snapshot.val().name);
+  $("#email-display").text(snapshot.val().email);
+  $("#age-display").text(snapshot.val().age);
+  $("#comment-display").text(snapshot.val().comment);
+
+  // Handle the errors
+}, function(errorObject) {
+  console.log("Errors handled: " + errorObject.code);
+});
